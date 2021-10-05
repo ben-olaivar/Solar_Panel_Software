4fdbf66928660f09936f08a411bd9e8a62ace8ed
Ben Olaivar <olaivar3@illinois.edu>
2021-10-05 15:06:58 -0500

    cleaned up repo

--- return.ps1
+++
@@ -1,7 +0,0 @@
-# This is Hyrum's personal codebase navigation script. It will be removed before the project is published.
-
-git pull
-git push
-conda deactivate
-Set-Location ..
-Set-Location powershell_scripts
